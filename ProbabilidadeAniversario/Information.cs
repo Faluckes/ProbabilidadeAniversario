b25e7081cf5b86e403a10e92476d08acad136d54
Faluckes <FaluckesDev@protonmail.com>
2022-03-10 21:56:36 -0300

    Adicionar arquivos de projeto.

---
+++ ProbabilidadeAniversario/Information.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbabilidadeAniversario
+{
+    public class Information
+    {
+        public int Pessoas { get; set; }
+        private int _aniversario { get; set; }
+        public double Aniversario
+        {
+            get
+            {
+                double p = Math.Pow((1.0 / 365), Pessoas);
+                for (double i = (366 - Pessoas); i < 366; i++)
+                {
+                    p *= i;
+                }
+
+                int porcentagem = Convert.ToInt16((1 - p) * 100);
+                return _aniversario = porcentagem;
+            }
+        }
+        public Information(int pessoas)
+        {
+            Pessoas = pessoas;
+            int _aniversario;
+        }
+
+
+    }
+}
---
+++ ProbabilidadeAniversario/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ProbabilidadeAniversario
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Projeto Probabilidade do Aniversario!");
+            Console.WriteLine("Aperte ENTER para continuar . . .");
+            Console.ReadLine();
+
+
+            Console.WriteLine("\n");
+            Console.Write("Coleque uma quantidade de pessoas que vai ter dentro de um salão: ");
+            string pessoas = Console.ReadLine();
+            Console.WriteLine($"\nAdicionado {pessoas} pessoas dentro do salão . . .");
+            Console.WriteLine($"\nQual a chance de {pessoas} pessoas fazerem aniversario no mesmo dia? Veremos a seguir!");
+            Console.WriteLine("Aperte ENTER para continuar. . . \n");
+            Console.ReadLine();
+
+            Information inf = new Information(Convert.ToInt16(pessoas));
+
+            Console.WriteLine($"{inf.Aniversario}% de {pessoas} pessoas podem fazer aniversario no mesmo dia. \n \n \n");
+            Console.WriteLine("Obrigado por testar meu Projeto! \n \n");
+            Console.WriteLine("Feito por Faluckes, fevereiro de 2022 \nhttps://github.com/Faluckes\n \n Aperte ENTER para fechar o programa.");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
